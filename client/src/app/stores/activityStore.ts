86130ddc1af92c71af91d5b52f49e1b6ce4de818
Johan Hanses <johanhanses@gmail.com>
2019-12-29 18:07:02 +0100

    Added some style using react-semantic-ui

--- client/src/app/stores/activityStore.ts
+++ client/src/app/stores/activityStore.ts
@@ -13,10 +13,27 @@ class ActivityStore {
     @observable target = "";
 
     @computed get activitiesdByDate() {
-        return Array.from(this.activityRegistry.values()).sort(
-            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+        return this.groupActivitiesByDate(
+            Array.from(this.activityRegistry.values())
         );
     }
+
+    groupActivitiesByDate(activities: IActivity[]) {
+        const sortedActivities = activities.sort(
+            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+        );
+        return Object.entries(
+            sortedActivities.reduce((activities, activity) => {
+                const date = activity.date.split("T")[0];
+                activities[date] = activities[date]
+                    ? [...activities[date], activity]
+                    : [activity];
+
+                return activities;
+            }, {} as { [key: string]: IActivity[] })
+        );
+    }
+
     @action loadActivities = async () => {
         this.loadingInitial = true;
         try {
@@ -28,6 +45,7 @@ class ActivityStore {
                 });
                 this.loadingInitial = false;
             });
+            console.log(this.groupActivitiesByDate(activities));
         } catch (error) {
             runInAction("load activities error", () => {
                 this.loadingInitial = false;
